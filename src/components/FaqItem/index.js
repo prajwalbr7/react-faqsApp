1bae1cfdea22d4f1622bfa124f881a06f61e30c8
Prajwal Yadav B R <78904638+prajwalbr7@users.noreply.github.com>
2023-03-26 19:39:55 +0530

    Update index.js

--- src/components/FaqItem/index.js
+++ src/components/FaqItem/index.js
@@ -1,39 +1,49 @@
 // Write your code here.
 import './index.css'
+import {Component} from 'react'
 
-const FaqItem = props => {
-  const {faqDetails, discription, onClickingAction} = props
-  const {questionText, answerText} = faqDetails
+class FaqItem extends Component {
+  state = {discription: false}
 
-  const imgToggle = discription
-    ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
-    : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
+  onClickingAction = () => {
+    this.setState(prevState => ({discription: !prevState.discription}))
+  }
 
-  const imgAlt = discription ? 'minus' : 'plus'
+  render() {
+    const {faqDetails} = this.props
+    const {questionText, answerText} = faqDetails
+    const {discription} = this.state
+    const imgToggle = discription
+      ? 'https://assets.ccbp.in/frontend/react-js/faqs-minus-icon-img.png'
+      : 'https://assets.ccbp.in/frontend/react-js/faqs-plus-icon-img.png'
 
-  const renderAnswer = discription ? (
-    <div>
-      <hr />
-      <p className="para-list-style">{answerText}</p>
-    </div>
-  ) : (
-    ''
-  )
+    const imgAlt = discription ? 'minus' : 'plus'
 
-  return (
-    <li className="list-container">
-      <div className="list-div-container-row">
-        <h1 className="heading-list ">{questionText}</h1>
-        <button
-          className="button-style-list"
-          type="button"
-          onClick={onClickingAction}
-        >
-          <img src={imgToggle} alt={imgAlt} className="" />
-        </button>
+    const renderAnswer = discription ? (
+      <div>
+        <hr />
+        <p className="para-list-style">{answerText}</p>
       </div>
-      {renderAnswer}
-    </li>
-  )
+    ) : (
+      ''
+    )
+
+    return (
+      <li className="list-container">
+        <div className="list-div-container-row">
+          <h1 className="heading-list ">{questionText}</h1>
+          <button
+            className="button-style-list"
+            type="button"
+            onClick={this.onClickingAction}
+          >
+            <img src={imgToggle} alt={imgAlt} className="" />
+          </button>
+        </div>
+        {renderAnswer}
+      </li>
+    )
+  }
 }
+
 export default FaqItem
